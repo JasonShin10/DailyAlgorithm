d2f753d02ee87e9fe7fff9eeb9e97be160a339b0
JasonShin <sjason2680@gmail.com>
2024-01-14 22:03:54 +0900

    Add BaekJoon_10808

---
+++ BaekJoon_10808.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int arr[26] = {};
+
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		arr[s[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
+
+
