785e3118c1e1b8c355c9c8345a9dcd408285f822
JasonShin <sjason2680@gmail.com>
2024-01-15 12:43:44 +0900

    Add BaekJoon_10988

---
+++ BaekJoon_10988.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	for (int i = 0; int j = s.size() - 1; i <= j;)
+	{
+		if (s[i] == s[j])
+		{
+			i++;
+			j--;
+		}
+		else
+		{
+			cout << 0;
+			return;
+		}
+	}
+	cout << 1;
+}
---
+++ BaekJoon_1159 - 복사본.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<char> arr;
+    int A[26] = {};
+    for (int i = 0; i < N; i++)
+    {
+        string s;
+        cin >> s;
+        A[s[0] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (A[i] > 4)
+        {
+            arr.push_back(i + 'a');
+        }
+    }
+    sort(arr.begin(), arr.end());
+    if (arr.empty())
+    {
+        cout << "PREDAJA";
+    }
+    else
+    {
+        for (int i = 0; i < arr.size(); i++)
+        {
+            cout << arr[i];
+        }
+    }
+}
