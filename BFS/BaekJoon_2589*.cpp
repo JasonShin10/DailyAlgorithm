670b90d668a082063b500f702573eabf9cd65c9f
JasonShin <101558745+JasonShin10@users.noreply.github.com>
2024-02-19 00:10:38 +0900

    Update BaekJoon_2589*.cpp

--- BFS/BaekJoon_2589*.cpp
+++ BFS/BaekJoon_2589*.cpp
@@ -12,44 +12,46 @@
 using namespace std;
 
 char arr[51][51] = {};
+int nArr[51][51] = {};
+
 bool visited[51][51] = {};
-int path[51][51] = {};
+
 queue<pair<int, int>> mq;
-int N, M;
-int mx = -1e9;
 
 int dx[] = { -1,1,0,0 };
 int dy[] = { 0,0,-1,1 };
+int N, M;
+int mx = -1e9;
 int ans = -1e9;
-// 처음 지역부터 목표 지역까지 몇번 걸리는지 
 void BFS(int x, int y)
 {
 	mq.push({ x,y });
+	nArr[x][y] =0;
 	visited[x][y] = true;
 	while (!mq.empty())
 	{
 		int x = mq.front().first;
 		int y = mq.front().second;
-
 		mq.pop();
+
+		if (mx < nArr[x][y])
+		{
+			mx = nArr[x][y];
+		}
+
 		for (int i = 0; i < 4; i++)
 		{
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
-			if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] == 'L' && visited[nx][ny] == false)
+			if (nx >= 0 && nx < N && ny >= 0 && ny < M && visited[nx][ny] == false && arr[nx][ny] == 'L')
 			{
-				visited[nx][ny] = true;
-				path[nx][ny] = path[x][y] + 1;
 				mq.push({ nx, ny });
-				if (mx < path[nx][ny])
-				{
-					mx = path[nx][ny];
-				}
+				nArr[nx][ny] = nArr[x][y] + 1;
+				visited[nx][ny] = true;
 			}
 		}
 	}
-
 }
 
 int main() {
@@ -73,16 +75,14 @@ int main() {
 			if (arr[i][j] == 'L')
 			{
 				BFS(i, j);
-				if (mx > ans)
+				if (ans < mx)
 				{
 					ans = mx;
 				}
 				memset(visited, 0, sizeof(visited));
-				memset(path, 0, sizeof(path));
 			}
 		}
 	}
 
 	cout << ans;
-
 }
