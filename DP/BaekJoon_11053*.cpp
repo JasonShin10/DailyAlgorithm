f392d72cd96b1d3b3512f381fc89e83c09814b6f
JasonShin <101558745+JasonShin10@users.noreply.github.com>
2024-04-15 00:52:46 +0900

    Create BaekJoon_11053*.cpp

---
+++ DP/BaekJoon_11053*.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <queue>
+#include <string>
+#include <math.h>
+#include <cstring>
+#include <stack>
+#include <cctype>
+
+using namespace std;
+int n;
+int arr[1004] = {};
+int dp[1004] = {};
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+		dp[i] = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+	
+	sort(dp, dp+n+1);
+	cout << dp[n];
+}
